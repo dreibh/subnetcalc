cae79ce7b32bee126c6a142ceec551ef60392d26
dreibh <dreibh@acdd8558-d4fd-0310-b0a5-8cf3c1ec824a>
2013-09-26 13:43:13 +0000

    Improved GeoIP output.
    git-svn-id: https://svn.tdr.iem.uni-due.de:8443/svn/sctp-misc/trunk/subnetcalc@1658 acdd8558-d4fd-0310-b0a5-8cf3c1ec824a

--- src/subnetcalc.cc
+++ src/subnetcalc.cc
@@ -1,7 +1,7 @@
 /* $Id$
  *
  * IPv4/IPv6 Subnet Calculator
- * Copyright (C) 2002-2010 by Thomas Dreibholz
+ * Copyright (C) 2002-2013 by Thomas Dreibholz
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -32,6 +32,7 @@
 #include <iostream>
 #ifdef HAVE_GEOIP
 #include <GeoIP.h>
+#include <GeoIPCity.h>
 #endif
 
 #include "tools.h"
@@ -214,7 +215,7 @@ void printAddressBinary(std::ostream&         os,
       int      p           = 0;
       in6_addr ipv6Address = getIPv6Address(address);
       for(int j = 0;j < 8;j++) {
-         uint16_t a = ntohs(((const uint16_t*)&ipv6Address.s6_addr)[j]);
+         uint16_t a = ntohs(ipv6Address.s6_addr[j]);
          char str[16];
          snprintf((char*)&str, sizeof(str), "%04x", a);
          os << indent << str << " = ";
@@ -411,28 +412,28 @@ void printUnicastProperties(std::ostream&   os,
    // ====== Global ID ======================================================
    if(hasGlobalID) {
       char           globalIDString[16];
-      const uint16_t globalID = ntohs(((const uint16_t*)&ipv6address.s6_addr)[3]);
+      const uint16_t globalID = ntohs((ipv6address.s6_addr)[3]);
       snprintf((char*)&globalIDString, sizeof(globalIDString), "%02x%04x%04x",
                ipv6address.s6_addr[1],
-               ntohs(((const uint16_t*)&ipv6address.s6_addr)[1]),
-               ntohs(((const uint16_t*)&ipv6address.s6_addr)[2]));
+               ntohs((ipv6address.s6_addr)[1]),
+               ntohs((ipv6address.s6_addr)[2]));
       os << "      + Global ID    = " << globalIDString << std::endl;
    }
 
    // ====== Subnet ID ======================================================
    if(hasSubnetID) {
       char           subnetIDString[16];
-      const uint16_t subnetID = ntohs(((const uint16_t*)&ipv6address.s6_addr)[3]);
+      const uint16_t subnetID = ntohs((ipv6address.s6_addr)[3]);
       snprintf((char*)&subnetIDString, sizeof(subnetIDString), "%04x", subnetID);
       os << "      + Subnet ID    = " << subnetIDString << std::endl;
    }
 
    // ====== Interface ID ===================================================
    char           interfaceIDString[128];
-   const uint16_t interfaceID[4] = { ntohs(((const uint16_t*)&ipv6address.s6_addr)[4]),
-                                     ntohs(((const uint16_t*)&ipv6address.s6_addr)[5]),
-                                     ntohs(((const uint16_t*)&ipv6address.s6_addr)[6]),
-                                     ntohs(((const uint16_t*)&ipv6address.s6_addr)[7]) };
+   const uint16_t interfaceID[4] = { ntohs((ipv6address.s6_addr)[4]),
+                                     ntohs((ipv6address.s6_addr)[5]),
+                                     ntohs((ipv6address.s6_addr)[6]),
+                                     ntohs((ipv6address.s6_addr)[7]) };
    snprintf((char*)&interfaceIDString, sizeof(interfaceIDString), "\x1b[36m%04x:%02x\x1b[37m%02x:%02x\x1b[38m%02x:%04x\x1b[0m",
             interfaceID[0],
             (interfaceID[1] & 0xff00) >> 8, (interfaceID[1] & 0x00ff),
@@ -456,8 +457,8 @@ void printUnicastProperties(std::ostream&   os,
    char snmcAddressString[32];
    snprintf((char*)&snmcAddressString, sizeof(snmcAddressString),
             "\x1b[32mff02::1:ff\x1b[38m%02x:%04x\x1b[0m",
-            ntohs(((const uint16_t*)&ipv6address.s6_addr)[6]) & 0xff,
-            ntohs(((const uint16_t*)&ipv6address.s6_addr)[7]));
+            ntohs((ipv6address.s6_addr)[6]) & 0xff,
+            ntohs((ipv6address.s6_addr)[7]));
    os << "      + Sol. Node MC = " << snmcAddressString << std::endl;
 }
 
@@ -568,8 +569,8 @@ void printAddressProperties(std::ostream&         os,
 
    // ====== IPv6 properties ================================================
    else {
-      const uint16_t a = ntohs(((const uint16_t*)&ipv6address.s6_addr)[0]);
-      const uint16_t b = ntohs(((const uint16_t*)&ipv6address.s6_addr)[1]);
+      const uint16_t a = ntohs((ipv6address.s6_addr)[0]);
+      const uint16_t b = ntohs((ipv6address.s6_addr)[1]);
 
       // ------ Special addresses -------------------------------------------
       if(IN6_IS_ADDR_LOOPBACK(&ipv6address)) {
@@ -635,13 +636,13 @@ void printAddressProperties(std::ostream&         os,
          // ------ Solicited node multicast address -------------------------
          // FF02::1:FF00:0/104
          if((a == 0xff02) &&
-            (ntohs(((const uint16_t*)&ipv6address.s6_addr)[5]) == 0x0001) &&
-            (ntohs(((const uint16_t*)&ipv6address.s6_addr)[6]) & 0xff00) == 0xff00) {
+            (ntohs((ipv6address.s6_addr)[5]) == 0x0001) &&
+            (ntohs((ipv6address.s6_addr)[6]) & 0xff00) == 0xff00) {
             char nodeAddressString[64];
             snprintf((char*)&nodeAddressString, sizeof(nodeAddressString),
                      "xxxx:xxxx:xxxx:xxxx:xxxx:xxxx:xx%02x:%04x",
-                     ntohs(((const uint16_t*)&ipv6address.s6_addr)[6]) >> 8,
-                     ntohs(((const uint16_t*)&ipv6address.s6_addr)[7]));
+                     ntohs((ipv6address.s6_addr)[6]) >> 8,
+                     ntohs((ipv6address.s6_addr)[7]));
             os << "      + Address is solicited node multicast address for "
                << nodeAddressString << std::endl;
          }
@@ -850,7 +851,14 @@ int main(int argc, char** argv)
    const char* country = NULL;
    const char* code    = NULL;
    if(address.sa.sa_family == AF_INET) {
-      GeoIP* geoIP = GeoIP_open_type(GEOIP_COUNTRY_EDITION, GEOIP_STANDARD);
+      GeoIP* geoIP = GeoIP_open_type(GEOIP_ASNUM_EDITION, GEOIP_STANDARD);
+      if(geoIP) {
+         const char* org = GeoIP_name_by_ipnum(geoIP, ntohl(address.in.sin_addr.s_addr));
+         std::cout << "GeoIP AS Info = "
+                   << ((org != NULL) ? org : "Unknown") << std::endl;
+         GeoIP_delete(geoIP);
+      }
+      geoIP = GeoIP_open_type(GEOIP_COUNTRY_EDITION, GEOIP_STANDARD);
       if(geoIP) {
          country = GeoIP_country_name_by_ipnum(geoIP, ntohl(address.in.sin_addr.s_addr));
          code    = GeoIP_country_code_by_ipnum(geoIP, ntohl(address.in.sin_addr.s_addr));
@@ -859,16 +867,67 @@ int main(int argc, char** argv)
                    << " (" << ((code != NULL) ? code : "??") << ")" << std::endl;
          GeoIP_delete(geoIP);
       }
+      geoIP = GeoIP_open_type(GEOIP_CITY_EDITION_REV1, GEOIP_STANDARD);
+      if(geoIP) {
+         GeoIPRecord* gir = GeoIP_record_by_ipnum(geoIP, ntohl(address.in.sin_addr.s_addr));
+         if(gir != NULL) {
+            const char* timeZone = GeoIP_time_zone_by_country_and_region(
+                                      gir->country_code, gir->region);
+            std::cout << "GeoIP Region  = "
+                      << ((gir->postal_code != NULL) ? gir->postal_code : "")
+                      << ((gir->postal_code != NULL) ? " " : "")
+                      << ((gir->city != NULL) ? gir->city : "Unknown")
+                      << ", " << ((gir->region != NULL) ? gir->region : "Unknown")
+                      << " ("
+                      << fabs(gir->latitude)  << "°" << ((gir->latitude > 0)  ? "N" : "S") << ", "
+                      << fabs(gir->longitude) << "°" << ((gir->longitude > 0) ? "E" : "W")
+                      << ((timeZone != NULL) ? ", " : "")
+                      << ((timeZone != NULL) ? timeZone : "")
+                      << ")"
+                      << std::endl;
+            GeoIPRecord_delete(gir);
+         }
+         GeoIP_delete(geoIP);
+      }
    }
 #ifdef HAVE_GEOIP_IPV6
    else if(address.sa.sa_family == AF_INET6) {
-      GeoIP* geoIP = GeoIP_open_type(GEOIP_COUNTRY_EDITION_V6, GEOIP_STANDARD);
+      GeoIP* geoIP = GeoIP_open_type(GEOIP_ASNUM_EDITION_V6, GEOIP_STANDARD);
+      if(geoIP) {
+         const char* org = GeoIP_name_by_ipnum_v6(geoIP, address.in6.sin6_addr);
+         std::cout << "GeoIP AS Info = "
+                   << ((org != NULL) ? org : "Unknown") << std::endl;
+         GeoIP_delete(geoIP);
+      }
+      geoIP = GeoIP_open_type(GEOIP_COUNTRY_EDITION_V6, GEOIP_STANDARD);
       if(geoIP) {
          country = GeoIP_country_name_by_ipnum_v6(geoIP, address.in6.sin6_addr);
          code    = GeoIP_country_code_by_ipnum_v6(geoIP, address.in6.sin6_addr);
          std::cout << "GeoIP Country = "
                    << ((country != NULL) ? country: "Unknown")
                    << " (" << ((code != NULL) ? code : "??") << ")" << std::endl;
+         GeoIP_delete(geoIP);
+      }
+      geoIP = GeoIP_open_type(GEOIP_CITY_EDITION_REV1_V6, GEOIP_STANDARD);
+      if(geoIP) {
+         GeoIPRecord* gir = GeoIP_record_by_ipnum_v6(geoIP, address.in6.sin6_addr);
+         if(gir != NULL) {
+            const char* timeZone = GeoIP_time_zone_by_country_and_region(
+                                      gir->country_code, gir->region);
+            std::cout << "GeoIP Region  = "
+                      << ((gir->postal_code != NULL) ? gir->postal_code : "")
+                      << ((gir->postal_code != NULL) ? " " : "")
+                      << ((gir->city != NULL) ? gir->city : "Unknown")
+                      << ", " << ((gir->region != NULL) ? gir->region : "Unknown")
+                      << " ("
+                      << fabs(gir->latitude)  << "°" << ((gir->latitude > 0)  ? "N" : "S") << ", "
+                      << fabs(gir->longitude) << "°" << ((gir->longitude > 0) ? "E" : "W")
+                      << ((timeZone != NULL) ? ", " : "")
+                      << ((timeZone != NULL) ? timeZone : "")
+                      << ")"
+                      << std::endl;
+            GeoIPRecord_delete(gir);
+         }
          GeoIP_delete(geoIP);
       }
    }
--- src/tools.h
+++ src/tools.h
@@ -1,7 +1,7 @@
 /* $Id$
  *
  * Network Performance Meter
- * Copyright (C) 2009-2010 by Thomas Dreibholz
+ * Copyright (C) 2009-2013 by Thomas Dreibholz
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
