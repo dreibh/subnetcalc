46fd97302a122714fa0f4a045c0ae735d219a7fc
dreibh <dreibh@acdd8558-d4fd-0310-b0a5-8cf3c1ec824a>
2015-03-01 10:42:18 +0000

    Updated debian/copyright to machine-readable format.
    git-svn-id: https://svn.tdr.iem.uni-due.de:8443/svn/sctp-misc/trunk/subnetcalc@1966 acdd8558-d4fd-0310-b0a5-8cf3c1ec824a

--- src/tools.cc
+++ src/tools.cc
@@ -1,7 +1,7 @@
 /* $Id$
  *
- * Network Performance Meter
- * Copyright (C) 2009-2013 by Thomas Dreibholz
+ * IPv4/IPv6 Subnet Calculator
+ * Copyright (C) 2009-2015 by Thomas Dreibholz
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
