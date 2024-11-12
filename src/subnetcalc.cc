b935c52f021458205dda525bbbb63f78684a5733
Thomas Dreibholz <dreibh@simula.no>
2024-11-12 09:47:16 +0100

    Added option to print program version.

--- src/subnetcalc.cc
+++ src/subnetcalc.cc
@@ -1,22 +1,31 @@
-/*
- * IPv4/IPv6 Subnet Calculator
- * Copyright (C) 2002-2024 by Thomas Dreibholz
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
-
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
- * Contact: thomas.dreibholz@gmail.com
- */
+// ==========================================================================
+//             ____        _     _   _      _    ____      _
+//            / ___| _   _| |__ | \ | | ___| |_ / ___|__ _| | ___
+//            \___ \| | | | '_ \|  \| |/ _ \ __| |   / _` | |/ __|
+//             ___) | |_| | |_) | |\  |  __/ |_| |__| (_| | | (__
+//            |____/ \__,_|_.__/|_| \_|\___|\__|\____\__,_|_|\___|
+//
+//                    ---  IPv4/IPv6 Subnet Calculator  ---
+//                   https://www.nntb.no/~dreibh/subnetcalc/
+// ==========================================================================
+//
+// SubNetCalc - IPv4/IPv6 Subnet Calculator
+// Copyright (C) 2024-2025 by Thomas Dreibholz
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+// Contact: thomas.dreibholz@gmail.com
 
 #include <assert.h>
 #include <libintl.h>
@@ -35,6 +44,7 @@
 #include <cstring>
 
 #include "tools.h"
+#include "package-version.h"
 
 
 #ifdef __FreeBSD__   // FreeBSD
@@ -84,6 +94,14 @@ inline bool isMulticast(const sockaddr_union& address)
       const in6_addr ipv6address = getIPv6Address(address);
       return IN6_IS_ADDR_MULTICAST(&ipv6address);
    }
+}
+
+
+// ###### Show version ######################################################
+void showVersion()
+{
+   std::cout << "SubNetCalc " << SUBNETCALC_VERSION << "\n";
+   exit(0);
 }
 
 
@@ -748,6 +766,11 @@ int main(int argc, char** argv)
       exit(1);
    }
 
+   // ====== Show version ===================================================
+   if((strcmp(argv[1], "-v") == 0) || (strcmp(argv[1], "-version") == 0) ) {
+      showVersion();
+   }
+
    // ====== Get address and netmask from one parameter =====================
    char* slash = index(argv[1], '/');
    if(slash) {
@@ -828,6 +851,10 @@ int main(int argc, char** argv)
       }
       else if(strcmp(argv[i], "-n") == 0) {
          noReverseLookup = true;
+      }
+      else if((strcmp(argv[i], "-v") == 0) ||
+              (strcmp(argv[i], "-version") == 0) ) {
+         showVersion();
       }
       else {
          std::cerr << format(gettext("ERROR: Invalid argument %s!"), argv[i]) << "\n";
--- src/tools.h
+++ src/tools.h
@@ -1,22 +1,31 @@
-/*
- * IPv4/IPv6 Subnet Calculator
- * Copyright (C) 2009-2024 by Thomas Dreibholz
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
-
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
- * Contact: thomas.dreibholz@gmail.com
- */
+// ==========================================================================
+//             ____        _     _   _      _    ____      _
+//            / ___| _   _| |__ | \ | | ___| |_ / ___|__ _| | ___
+//            \___ \| | | | '_ \|  \| |/ _ \ __| |   / _` | |/ __|
+//             ___) | |_| | |_) | |\  |  __/ |_| |__| (_| | | (__
+//            |____/ \__,_|_.__/|_| \_|\___|\__|\____\__,_|_|\___|
+//
+//                    ---  IPv4/IPv6 Subnet Calculator  ---
+//                   https://www.nntb.no/~dreibh/subnetcalc/
+// ==========================================================================
+//
+// SubNetCalc - IPv4/IPv6 Subnet Calculator
+// Copyright (C) 2024-2025 by Thomas Dreibholz
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+// Contact: thomas.dreibholz@gmail.com
 
 #ifndef TOOLS_H
 #define TOOLS_H
