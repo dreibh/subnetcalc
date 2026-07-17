3c08e131a48408b5054830ad1fe4eb2eb954a644
Thomas Dreibholz <dreibh@simula.no>
2026-07-17 15:07:52 +0200

    Added initial NetBSD port files.

---
+++ packaging/netbsd/options.mk
@@ -0,0 +1,30 @@
+# $NetBSD$
+
+PKG_OPTIONS_VAR=		PKG_OPTIONS.subnetcalc
+PKG_SUPPORTED_OPTIONS=		i18n geoip
+PKG_SUGGESTED_OPTIONS=		i18n geoip
+
+.include "../../mk/bsd.options.mk"
+
+PLIST_VARS+=			i18n
+
+###
+### Internationalization (i18n) support
+###
+.if !empty(PKG_OPTIONS:Mi18n)
+CMAKE_CONFIGURE_ARGS+=		-DWITH_I18N=ON
+.include "../../devel/gettext-lib/buildlink3.mk"
+PLIST.i18n=			yes
+.else
+CMAKE_CONFIGURE_ARGS+=		-DWITH_I18N=OFF
+.endif
+
+###
+### GeoIP support
+###
+.if !empty(PKG_OPTIONS:Mgeoip)
+CMAKE_CONFIGURE_ARGS+=		-DWITH_GEOIP=ON
+.include "../../net/libmaxminddb/buildlink3.mk"
+.else
+CMAKE_CONFIGURE_ARGS+=		-DWITH_GEOIP=OFF
+.endif
