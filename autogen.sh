66dce35453243d166960f9a9f0b10fa62a4661b6
Thomas Dreibholz <dreibh@simula.no>
2026-07-18 15:13:09 +0200

    Updated build helper script.

--- autogen.sh
+++ autogen.sh
@@ -120,7 +120,7 @@ if [ "${CORES}" -lt 1 ] ; then
    elif [ "${UNAME}" == "FreeBSD" ] ; then
       CORES="$(sysctl -n hw.ncpu || echo "1")"
    elif [ "${UNAME}" == "NetBSD" ] || [ "${UNAME}" == "OpenBSD" ] ; then
-      CORES="$(sysctl -n hw.ncpuonline || echo "1")"
+      CORES="$(/sbin/sysctl -n hw.ncpuonline || echo "1")"
    elif [ "${UNAME}" == "SunOS" ] ; then
       CORES="$(psrinfo -t)"
    elif [ "${UNAME}" == "Darwin" ] ; then
